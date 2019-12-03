54d2719b0011c271c9a42f907ec95cae0fca2c79
aisair <ais11air@gmail.com>
2019-12-03 11:05:26 -0600

    Frogger: Added roads and grass to background

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let background = new Image();
 let frog = new Image();
 let car = new Image();
 
 function init() { //purpose: push image into array and use it by calling the array with an index.
   frog.src = "img/frog.png";
-  background.src = "img/background.png";
   drawBackground();
 }
 
 function start() {
   //change button to stop game
   let button = document.getElementById("toggle");
-  button.innerHTML = "Click to Stop Game";
+  button.innerHTML = "Stop Game";
   button.setAttribute("onclick", "stop()");
 
   window.requestAnimationFrame(animate);
@@ -22,7 +20,7 @@ function start() {
 
 function stop() {
   let button = document.getElementById("toggle");
-  button.innerHTML = "Click to Start Game";
+  button.innerHTML = "Start Game";
   button.setAttribute("onclick", "start()");
 
   cancelAnimationFrame(animate);
@@ -35,7 +33,12 @@ function animate() {//animations will run at fps of computer
 }
 
 function drawBackground() {
-  ctx.drawImage(background, 0, 0, 500, 500);
+  ctx.fillStyle = "lime"; //draw grass
+  ctx.fillRect(0, 0, 500, 500);
+  ctx.fillStyle = "black"; //draw roads
+  for (let i = 100; i < 400; i += 50) {
+    ctx.fillRect(0, i, 500, 49);
+  }
 }
 
 function drawImages() {
