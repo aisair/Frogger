5a6e876ed2a6edfd1bfddf0143687af5e95dee41
aisair <ais11air@gmail.com>
2019-12-04 11:05:45 -0600

    Frogger Added functionality

--- js/main.js
+++ js/main.js
@@ -1,50 +1,78 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let a;
 
 let frog = new Image();
-let car = new Image();
+let car = [];
+let frogX = 250;
+let frogY = 450;
+let win = 0;
 
 function init() { //purpose: push image into array and use it by calling the array with an index.
   frog.src = "img/frog.png";
+  car.push()
   drawBackground();
 }
 
 function start() {
   //change button to stop game
   let button = document.getElementById("toggle");
-  button.innerHTML = "Stop Game";
+  let image = document.getElementById("toggleImage");
+  image.setAttribute("src", "img/pause.png");
   button.setAttribute("onclick", "stop()");
 
-  window.requestAnimationFrame(animate);
+  a = window.requestAnimationFrame(animate);
 }
 
 function stop() {
   let button = document.getElementById("toggle");
-  button.innerHTML = "Start Game";
+  let image = document.getElementById("toggleImage");
+  image.setAttribute("src", "img/play.png");
   button.setAttribute("onclick", "start()");
 
-  cancelAnimationFrame(animate);
+  cancelAnimationFrame(a);
 }
 
 function animate() {//animations will run at fps of computer
   drawBackground();
   drawImages();
-  window.requestAnimationFrame(animate);
+
+  a = window.requestAnimationFrame(animate);
 }
 
+$(document).keydown(function(event){
+  switch (event.keyCode ? event.keyCode : event.which){
+    case 37:
+      frogX-=49;
+    break;
+    case 38:
+      frogY-=34;
+    break;
+    case 39:
+      frogX+=49;
+    break;
+    case 40:
+      frogY+=34;
+    break;
+  }
+  if (frogY <= 50){
+    win = 1;
+  }
+});
+
 function drawBackground() {
-  ctx.fillStyle = "lime"; //draw grass
+  //draw grass
+  ctx.fillStyle = "lime";
   ctx.fillRect(0, 0, 500, 500);
-  ctx.fillStyle = "black"; //draw roads
+  //draw roads
+  ctx.fillStyle = "black";
   for (let i = 100; i < 400; i += 50) {
     ctx.fillRect(0, i, 500, 49);
   }
 }
 
 function drawImages() {
-  ctx.drawImage(frog, 100, 100);
+  ctx.drawImage(frog, frogX - frog.width / 2, frogY - frog.height / 2);
 }
 
-window.onload = function() {
-  init()
-};
+window.onload = function() {init()};
